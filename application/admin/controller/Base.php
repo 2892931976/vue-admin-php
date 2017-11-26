a8c647ac645bf789fed1d43c935cb82362f6b22a
lmxdawn@qq.com <lmxdawn@qq.com>
2017-11-26 22:13:42 +0800

    第一次提交

---
+++ application/admin/controller/Base.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\admin\controller;
+
+use think\Controller;
+
+/**
+ * 基础控制器
+ */
+
+class Base extends Controller
+{
+
+    public function initialize()
+    {
+        parent::initialize();
+        header('Access-Control-Allow-Origin:*');
+        header('Access-Control-Allow-Methods:GET,POST,PUT,DELETE,PATCH,OPTIONS');
+        header('Access-Control-Allow-Headers:Content-Type, X-ELEME-USERID, X-Eleme-RequestID, X-Shard,X-Shard, X-Eleme-RequestID');
+    }
+}
